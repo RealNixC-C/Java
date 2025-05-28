d2b49922afff0f9845c7c1aeb331cfd2d8aee72d
RealNixC-C <john7622@naver.com>
2025-05-28 17:57:11 +0900

    up

--- chap11/src/sec01/homework_string/homework05/Run.java
+++ chap11/src/sec01/homework_string/homework05/Run.java
@@ -1,10 +1,19 @@
 package sec01.homework_string.homework05;
 
+import java.util.Scanner;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+		System.out.print("생년월일 : ");
+		String birth = sc.nextLine();
+		
+		
 	}
 
 }
